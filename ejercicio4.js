a8aa8b552050cf8acceb43d7bba41f64b3614429
Luis <lgerardochv@gmail.com>
2025-10-22 12:14:23 -0500

    Ejercicio 4

---
+++ ejercicio4.js
@@ -0,0 +1,6 @@
+console.log("Ejercicio 4");
+let numero = parseInt(prompt("Ingrese un numero: "));
+for (let i=1 ; i <= 12 ; i++){
+    let multiplicar = i * numero;
+    console.log(numero + " * " + i + " = " + multiplicar);
+}
