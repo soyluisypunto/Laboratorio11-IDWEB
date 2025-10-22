21636363666792e3cb20eddaf4f6a734e7a25e66
Luis <lgerardochv@gmail.com>
2025-10-22 12:13:38 -0500

    Ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+let nota1 = 12.5;
+let nota2 = 11;
+let nota3 = 10;
+let nota4 = 20;
+let nota5 = 15.6;
+
+const promedio = (nota1 + nota2 + nota3 + nota4 + nota5)/5
+
+console.log("Ejercicio 1:");
+console.log("El promedio de las 5 notas es: "+promedio);
